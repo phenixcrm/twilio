c0518a5315cbdc073ba260a906f82c0743b610d7
Michael Ventura <michael.ventura@gmail.com>
2022-07-08 17:31:52 -0400

    process digi task timeout

--- src/main/java/com/ameriglide/phenix/twilio/Events.java
+++ src/main/java/com/ameriglide/phenix/twilio/Events.java
@@ -1,5 +1,6 @@
 package com.ameriglide.phenix.twilio;
 
+import com.ameriglide.phenix.common.Agent;
 import com.ameriglide.phenix.common.Call;
 import com.ameriglide.phenix.servlet.TwiMLServlet;
 import com.ameriglide.phenix.types.Resolution;
@@ -11,6 +12,9 @@ import net.inetalliance.log.Log;
 import net.inetalliance.potion.Locator;
 import net.inetalliance.types.json.JsonMap;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
 @WebServlet("/twilio/events")
 public class Events extends TwiMLServlet {
   @Override
@@ -18,12 +22,22 @@ public class Events extends TwiMLServlet {
     var task = JsonMap.parse(request.getParameter("TaskAttributes"));
     switch (request.getParameter("EventType")) {
       case "task.cancelled" -> {
-        var call = Locator.$(new Call(task.get("VoiceCall")));
-        log.info("%s cancelled (%s)", call.sid, request.getParameter("Reason"));
-        Startup.router.sendToVoicemail(call.sid);
-        Locator.update(call, "Events", copy -> {
-          copy.setResolution(Resolution.VOICEMAIL);
-        });
+        if(task.containsKey("VoiceCall")) {
+          var call = Locator.$(new Call(task.get("VoiceCall")));
+          log.info("%s cancelled (%s)", call.sid, request.getParameter("Reason"));
+          Startup.router.sendToVoicemail(call.sid);
+          Locator.update(call, "Events", copy -> {
+            copy.setResolution(Resolution.VOICEMAIL);
+          });
+        } else if (task.containsKey("Lead")) {
+          var call = Locator.$(new Call(request.getParameter("TaskSid")));
+          Locator.update(call,"Events", copy-> {
+            copy.setResolution(Resolution.DROPPED);
+            copy.setBlame(Agent.system());
+            copy.setDuration(ChronoUnit.SECONDS.between(copy.getCreated(), LocalDateTime.now()));
+            copy.setTalkTime(0);
+          });
+        }
       }
     }
     return null;
