c61144b387fbb04e4991679423d90f7953d795b3
Michael Ventura <michael.ventura@gmail.com>
2022-07-31 21:11:03 -0400

    upgrade and sync upstream 3rd party dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,8 +9,8 @@ repositories {
 }
 
 dependencies {
-  testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+  testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
   implementation(project(":common"))
   implementation("net.inetalliance:potion:6.1-SNAPSHOT")
   implementation("net.inetalliance:types:1.1-SNAPSHOT")
@@ -23,11 +23,11 @@ dependencies {
   implementation("net.inetalliance.msg:aj:1.1-SNAPSHOT")
   implementation("org.apache.commons:commons-csv:1.9.0")
   implementation("net.inetalliance.msg:bjx:6.1-SNAPSHOT")
-  implementation("org.postgresql:postgresql:42.3.4")
+  implementation("org.postgresql:postgresql:42.3.6")
   implementation("jakarta.servlet:jakarta.servlet-api:5.0.0")
   implementation("jakarta.websocket:jakarta.websocket-api:2.0.0")
-  implementation("com.twilio.sdk:twilio:8.29.1")
-  implementation("io.github.cdimascio:dotenv-java:2.2.3")
+  implementation("com.twilio.sdk:twilio:8.34.0")
+  implementation("io.github.cdimascio:dotenv-java:2.2.4")
 }
 
 tasks.getByName<Test>("test") {
