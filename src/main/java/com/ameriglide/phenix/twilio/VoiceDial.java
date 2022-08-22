d75bfcba7605f3a22d4f16181ce2080fe0248719
Michael Ventura <michael.ventura@gmail.com>
2022-08-22 12:04:19 -0400

    deal with URL encoded params

--- src/main/java/com/ameriglide/phenix/twilio/VoiceDial.java
+++ src/main/java/com/ameriglide/phenix/twilio/VoiceDial.java
@@ -14,53 +14,56 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import net.inetalliance.potion.Locator;
 
+import java.nio.charset.StandardCharsets;
+
+import static java.net.URLDecoder.decode;
 import static net.inetalliance.funky.StringFun.isEmpty;
 
 @WebServlet("/twilio/voice/dial")
 public class VoiceDial extends TwiMLServlet {
-  @Override
-  protected TwiML getResponse(HttpServletRequest request, HttpServletResponse response) {
-    var agent = request.getParameter("agent");
-    var number = request.getParameter("number");
-    Party called;
-    if (isEmpty(agent)) {
-      if (isEmpty(number)) {
-        throw new IllegalArgumentException();
-      }
-      called = asParty(new PhoneNumber(number));
-    } else {
-      called = asParty(Locator.$(new Agent(Integer.parseInt(agent))));
+    @Override
+    protected TwiML getResponse(HttpServletRequest request, HttpServletResponse response) {
+        var agent = request.getParameter("agent");
+        var number = decode(request.getParameter("number"), StandardCharsets.UTF_8);
+        Party called;
+        if (isEmpty(agent)) {
+            if (isEmpty(number)) {
+                throw new IllegalArgumentException();
+            }
+            called = asParty(new PhoneNumber(number));
+        } else {
+            called = asParty(Locator.$(new Agent(Integer.parseInt(agent))));
+        }
+        // call the caller and then connect to the called party
+        var call = Locator.$(new Call(request.getParameter("CallSid")));
+        if (call==null) {
+            throw new NotFoundException();
+        }
+        if (called.isAgent()) {
+            // internal call
+            return new VoiceResponse.Builder()
+                    .say(speak("Connecting you to " + called.agent().getFullName()))
+                    .dial(new Dial.Builder()
+                            .action("/twilio/voice/postDial")
+                            .method(HttpMethod.GET)
+                            .answerOnBridge(true)
+                            .timeout(15)
+                            .sip(buildSip(called))
+                            .build())
+                    .build();
+        } else {
+            // outbound call
+            return new VoiceResponse.Builder()
+                    .say(speak("Dialing the number you requested"))
+                    .dial(new Dial.Builder()
+                            .action("/twilio/voice/postDial")
+                            .method(HttpMethod.GET)
+                            .answerOnBridge(true)
+                            .timeout(60)
+                            .number(buildNumber(asParty(new PhoneNumber(called.endpoint()))))
+                            .callerId(call.getPhone())
+                            .build())
+                    .build();
+        }
     }
-    // call the caller and then connect to the called party
-    var call = Locator.$(new Call(request.getParameter("CallSid")));
-    if (call == null) {
-      throw new NotFoundException();
-    }
-    if (called.isAgent()) {
-      // internal call
-      return new VoiceResponse.Builder()
-        .say(speak("Connecting you to " + called.agent().getFullName()))
-        .dial(new Dial.Builder()
-          .action("/twilio/voice/postDial")
-          .method(HttpMethod.GET)
-          .answerOnBridge(true)
-          .timeout(15)
-          .sip(buildSip(called))
-          .build())
-        .build();
-    } else {
-      // outbound call
-      return new VoiceResponse.Builder()
-        .say(speak("Dialing the number you requested"))
-        .dial(new Dial.Builder()
-          .action("/twilio/voice/postDial")
-          .method(HttpMethod.GET)
-          .answerOnBridge(true)
-          .timeout(60)
-          .number(buildNumber(asParty(new PhoneNumber(called.endpoint()))))
-          .callerId(call.getPhone())
-          .build())
-        .build();
-    }
-  }
 }
