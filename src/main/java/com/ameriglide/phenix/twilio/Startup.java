432ce57452e9f0ea40721dc2c5f1818372e1b950
Michael Ventura <michael.ventura@gmail.com>
2022-09-14 12:35:03 -0400

    add dynamic log config reloading

--- src/main/java/com/ameriglide/phenix/twilio/Startup.java
+++ src/main/java/com/ameriglide/phenix/twilio/Startup.java
@@ -1,8 +1,8 @@
 package com.ameriglide.phenix.twilio;
 
 import com.ameriglide.phenix.common.Agent;
+import com.ameriglide.phenix.core.ExecutorServices;
 import com.ameriglide.phenix.core.Log;
-import com.ameriglide.phenix.servlet.PhenixServlet;
 import com.ameriglide.phenix.twilio.tasks.SyncWorkerStatus;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.annotation.WebListener;
@@ -58,7 +58,7 @@ public class Startup extends com.ameriglide.phenix.servlet.Startup {
     @Override
     public void contextDestroyed(final ServletContextEvent sce) {
         super.contextDestroyed(sce);
-        PhenixServlet.shutdown("Midnight Logout",executor);
+        ExecutorServices.shutdown("Midnight Logout",executor);
 
     }
 }
