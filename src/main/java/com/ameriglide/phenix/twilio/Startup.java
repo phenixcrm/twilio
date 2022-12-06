9a62109e0fb707e308067b6dca4b0edb9962f8d6
Michael Ventura <michael.ventura@gmail.com>
2022-12-06 16:57:30 -0500

    smarter call closer logic

--- src/main/java/com/ameriglide/phenix/twilio/Startup.java
+++ src/main/java/com/ameriglide/phenix/twilio/Startup.java
@@ -1,9 +1,11 @@
 package com.ameriglide.phenix.twilio;
 
 import com.ameriglide.phenix.common.Agent;
+import com.ameriglide.phenix.common.Call;
 import com.ameriglide.phenix.core.ExecutorServices;
 import com.ameriglide.phenix.core.Log;
 import com.ameriglide.phenix.core.Optionals;
+import com.ameriglide.phenix.core.Strings;
 import com.ameriglide.phenix.servlet.topics.HudTopic;
 import com.ameriglide.phenix.twilio.tasks.SyncWorkerSkills;
 import com.ameriglide.phenix.types.Resolution;
@@ -15,11 +17,14 @@ import net.inetalliance.potion.Locator;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 import static com.ameriglide.phenix.common.Call.isActiveVoiceCall;
+import static com.twilio.rest.api.v2010.account.Call.Status.*;
+import static java.util.stream.Collectors.toSet;
 import static net.inetalliance.potion.Locator.forEach;
 
 @WebListener
@@ -48,23 +53,33 @@ public class Startup extends com.ameriglide.phenix.servlet.Startup {
 
     }, secs, TimeUnit.SECONDS);
     executor.scheduleWithFixedDelay(() -> {
-      var closed = new HashSet<>();
-      forEach(isActiveVoiceCall, call -> {
-          var twilioCall = router.getCall(call.sid);
-          var status = twilioCall.getStatus();
-          switch (status) {
-            case QUEUED, RINGING, IN_PROGRESS -> {
-              log.trace(() -> "%s is still in progress [%s]".formatted(call.sid, status));
-            }
-            case COMPLETED, BUSY, FAILED, NO_ANSWER, CANCELED -> {
-              Locator.update(call, "Closer", copy -> {
-                log.info(() -> "Closed stuck call %s for %s".formatted(call.sid,
-                  Optionals.of(call.getActiveAgent()).map(Agent::getFullName).orElse("nobody")));
-                copy.setResolution(Resolution.DROPPED);
-              });
-              closed.add(call.sid);
-            }
+      var open = new HashSet<String>();
+      forEach(isActiveVoiceCall, c -> open.add(c.sid));
+      shared.availability().values().stream().map(s -> s.call).filter(Strings::isNotEmpty).forEach(open::add);
+      var stuck = new HashSet<Integer>();
+      var closedStates = Set.of(COMPLETED, BUSY, FAILED, NO_ANSWER, CANCELED);
+      var closed = open
+        .stream()
+        .filter(sid -> {
+          var call =  router.getCall(sid);
+          return call == null || closedStates.contains(call.getStatus());
+        })
+        .collect(toSet());
+
+
+
+      open.forEach(sid -> {
+        var twilioCall = router.getCall(sid);
+        var status = twilioCall.getStatus();
+        switch (status) {
+          case QUEUED, RINGING, IN_PROGRESS -> {
+            log.trace(() -> "%s is still in progress [%s]".formatted(sid, status));
           }
+          case COMPLETED, BUSY, FAILED, NO_ANSWER, CANCELED -> {
+
+            closed.add(sid);
+          }
+        }
       });
       if (!closed.isEmpty()) {
         log.info(() -> "requesting hud refresh after stuck calls closed");
@@ -73,6 +88,16 @@ public class Startup extends com.ameriglide.phenix.servlet.Startup {
           .values()
           .stream()
           .filter(status -> status.call!=null && closed.contains(status.call))
+          .peek(s-> {
+            var call = Locator.$(new Call(s.call));
+            if(call != null) {
+              Locator.update(call, "Closer", copy -> {
+                log.info(() -> "closed stuck call %s for %s".formatted(call.sid,
+                  Optionals.of(call.getActiveAgent()).map(Agent::getFullName).orElse("nobody")));
+                copy.setResolution(Resolution.DROPPED);
+              });
+            }
+          })
           .map(s -> s.id)
           .map(Agent::new)
           .map(Locator::$)
