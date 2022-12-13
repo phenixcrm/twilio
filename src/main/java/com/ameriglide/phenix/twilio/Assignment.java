45e3c2b7f5e5a273739bbae2b9f6fa34772d2449
Michael Ventura <michael.ventura@gmail.com>
2022-12-12 22:30:11 -0500

    accept reservations quickly by creating tasks in the event loop instead of assignment

--- src/main/java/com/ameriglide/phenix/twilio/Assignment.java
+++ src/main/java/com/ameriglide/phenix/twilio/Assignment.java
@@ -3,21 +3,15 @@ package com.ameriglide.phenix.twilio;
 import com.ameriglide.phenix.common.Agent;
 import com.ameriglide.phenix.common.AgentStatus;
 import com.ameriglide.phenix.common.Call;
-import com.ameriglide.phenix.common.Opportunity;
 import com.ameriglide.phenix.core.Log;
-import com.ameriglide.phenix.core.Optionals;
 import com.ameriglide.phenix.servlet.PhenixServlet;
-import com.ameriglide.phenix.servlet.Startup;
-import com.twilio.type.PhoneNumber;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import net.inetalliance.potion.Locator;
 import net.inetalliance.types.json.JsonMap;
 
 import static com.ameriglide.phenix.servlet.Startup.shared;
 import static com.ameriglide.phenix.servlet.Startup.topics;
-import static net.inetalliance.potion.Locator.update;
 
 @WebServlet("/twilio/assignment")
 public class Assignment extends PhenixServlet {
@@ -39,35 +33,7 @@ public class Assignment extends PhenixServlet {
 
   @Override
   protected void post(HttpServletRequest request, HttpServletResponse response) throws Exception {
-    var reservation = request.getParameter("ReservationSid");
-    var task = request.getParameter("TaskSid");
-    var agent = Locator.$1(Agent.withSid(request.getParameter("WorkerSid")));
-    var attributes = JsonMap.parse(request.getParameter("TaskAttributes"));
-    var callSid = Optionals.of(attributes.get("call_sid")).orElse(task);
-    log.info(() -> "ASSIGN %s %s to %s, Reservation=%s".formatted(callSid, attributes.get("caller"),
-      agent.getFullName(),reservation));
-    var call = Locator.$(new Call(callSid));
-    update(call, "Assignment", copy -> {
-      copy.setAgent(agent);
-      copy.setBlame(agent);
-    });
-    if (attributes.containsKey("VoiceCall")) {
-      Startup.router.voiceTask(task, reservation, new PhoneNumber(attributes.get("caller")), agent, callSid);
-      response.sendError(200);
-    } else {
-      var opp = attributes.containsKey("Lead") ? Locator.$(
-        new Opportunity(Integer.valueOf(attributes.get("Lead")))):call.getOpportunity();
-      if (opp==null) {
-        log.error(() -> "Could not find opp for assignment: %s".formatted(attributes));
-      } else if (Agent.system().id.equals(opp.getAssignedTo().id)) {
-        update(opp, "Assignment", copy -> {
-          copy.setAssignedTo(agent);
-        });
-      }
-      PhenixServlet.respond(response, new JsonMap().$("instruction", "accept"));
-    }
-    pop(agent, callSid);
-    notify(call);
+
   }
 
   public static void pop(Agent agent, String callSid) {
