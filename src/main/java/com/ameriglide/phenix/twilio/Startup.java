6827d7d987d4e51848470c618c0bb77ec5a451a7
Michael Ventura <michael.ventura@gmail.com>
2023-02-08 21:55:14 -0500

    stop the unneeded call closer

--- src/main/java/com/ameriglide/phenix/twilio/Startup.java
+++ src/main/java/com/ameriglide/phenix/twilio/Startup.java
@@ -1,31 +1,18 @@
 package com.ameriglide.phenix.twilio;
 
-import com.ameriglide.phenix.common.Agent;
-import com.ameriglide.phenix.common.Call;
 import com.ameriglide.phenix.core.ExecutorServices;
 import com.ameriglide.phenix.core.Log;
-import com.ameriglide.phenix.core.Optionals;
-import com.ameriglide.phenix.core.Strings;
-import com.ameriglide.phenix.servlet.topics.HudTopic;
-import com.ameriglide.phenix.types.Resolution;
 import com.twilio.rest.taskrouter.v1.workspace.Worker;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.annotation.WebListener;
-import net.inetalliance.potion.Locator;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-import static com.ameriglide.phenix.common.Call.isActiveVoiceCall;
 import static com.ameriglide.phenix.types.WorkerState.OFFLINE;
-import static com.twilio.rest.api.v2010.account.Call.Status.*;
-import static java.util.stream.Collectors.toSet;
-import static net.inetalliance.potion.Locator.forEach;
 
 @WebListener
 public class Startup extends com.ameriglide.phenix.servlet.Startup {
@@ -52,44 +39,7 @@ public class Startup extends com.ameriglide.phenix.servlet.Startup {
       schedule();
 
     }, secs, TimeUnit.SECONDS);
-    executor.scheduleWithFixedDelay(() -> {
-      var open = new HashSet<String>();
-      forEach(isActiveVoiceCall, c -> open.add(c.sid));
-      shared.availability().values().stream().map(s -> s.call).filter(Strings::isNotEmpty).forEach(open::add);
-      var closedStates = Set.of(COMPLETED, BUSY, FAILED, NO_ANSWER, CANCELED);
-      var missing = new HashSet<>();
-      var closed = open.stream().filter(sid -> {
-        var call = router.getCall(sid);
-        if(call == null) {
-          missing.add(sid);
-        }
-        return call==null || closedStates.contains(call.getStatus());
-      }).collect(toSet());
 
-      if (!closed.isEmpty()) {
-        log.info(() -> "requesting hud refresh after stuck calls closed");
-        shared
-          .availability()
-          .values()
-          .stream()
-          .filter(status -> status.call!=null && closed.contains(status.call))
-          .peek(s -> {
-            var call = Locator.$(new Call(s.call));
-            if (call!=null) {
-              Locator.update(call, "Closer", copy -> {
-                log.info(() -> "closed stuck call %s for %s".formatted(call.sid,
-                  Optionals.of(call.getActiveAgent()).map(Agent::getFullName).orElse("nobody")));
-                copy.setResolution(Resolution.DROPPED);
-              });
-            }
-          })
-          .map(s -> s.id)
-          .map(Agent::new)
-          .map(Locator::$)
-          .forEach(Assignment::clear);
-        Startup.topics.hud().publish(HudTopic.PRODUCE);
-      }
-    }, 0, 60, TimeUnit.SECONDS);
 
   }
 
