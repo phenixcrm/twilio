3927b20fa7e465dfcde894c232a7d6556e1100c7
Michael Ventura <michael.ventura@gmail.com>
2023-10-26 14:06:09 -0400

    only publish pop events once per call sid/agent pair

--- src/main/java/com/ameriglide/phenix/twilio/Assignment.java
+++ src/main/java/com/ameriglide/phenix/twilio/Assignment.java
@@ -9,6 +9,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import net.inetalliance.types.json.JsonMap;
 
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
 import static com.ameriglide.phenix.servlet.Startup.topics;
 
 @WebServlet("/twilio/assignment")
@@ -22,12 +26,38 @@ public class Assignment extends PhenixServlet {
   protected void post(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
   }
+  record Pop (String callSid, Integer agent) {
+    @Override
+    public boolean equals(final Object o) {
+      if (this==o) {
+        return true;
+      }
+      if (o==null || getClass()!=o.getClass()) {
+        return false;
+      }
+      final Pop pop = (Pop) o;
+      return Objects.equals(callSid, pop.callSid) && Objects.equals(agent, pop.agent);
+    }
+    Pop(String callSid, Agent agent) {
+      this(callSid,agent.id);
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(callSid, agent);
+    }
+  }
+  static final Set<Pop> popped = new HashSet<>();
 
   public static void pop(Agent agent, String callSid) {
-    log.debug(()->"popping %s for %s".formatted(callSid,agent.getFullName()));
-    topics
-      .events()
-      .publishAsync(new JsonMap().$("agent", agent.id).$("type", "pop").$("event", new JsonMap().$("callId", callSid)));
+    if(popped.add(new Pop(callSid,agent))) {
+      log.debug(() -> "popping %s for %s".formatted(callSid, agent.getFullName()));
+      topics
+        .events()
+        .publishAsync(new JsonMap().$("agent", agent.id).$("type", "pop").$("event", new JsonMap().$("callId", callSid)));
+    } else {
+      log.debug(()->"not popping %s for %s, because we've done that already".formatted(callSid,agent.getFullName()));
+    }
   }
 
   public static void notify(final Call call) {
