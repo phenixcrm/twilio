c4f5b6e68c55512c38c37bf47ec44319d814834b
Michael Ventura <michael.ventura@gmail.com>
2024-03-07 00:08:32 -0500

    working cold transfers to queues, with default queues for teams in the dial button

--- src/main/java/com/ameriglide/phenix/twilio/voice/Transfer.java
+++ src/main/java/com/ameriglide/phenix/twilio/voice/Transfer.java
@@ -1,5 +1,6 @@
 package com.ameriglide.phenix.twilio.voice;
 
+import com.ameriglide.phenix.DialMode;
 import com.ameriglide.phenix.common.*;
 import com.ameriglide.phenix.servlet.TwiMLServlet;
 import com.ameriglide.phenix.servlet.exception.NotFoundException;
@@ -12,7 +13,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import net.inetalliance.potion.Locator;
 
-import static com.ameriglide.phenix.core.Strings.isEmpty;
 import static com.ameriglide.phenix.servlet.TwiMLServlet.Op.CREATE;
 import static com.ameriglide.phenix.servlet.TwiMLServlet.Op.THROW;
 
@@ -25,40 +25,48 @@ public class Transfer extends TwiMLServlet {
   @Override
   protected void get(final HttpServletRequest request, final HttpServletResponse response, Call call, Leg leg) throws
     Exception {
-    var agentId = request.getParameter("agent");
-    var number = request.getParameter("number");
-    if (isEmpty(agentId)) {
-      if (isEmpty(number)) {
-        throw new IllegalArgumentException("Must provide agent or number parameter");
-      } else {
+    var param = DialMode.fromRequest(request);
+    var agentId = param.value();
+    switch (param.mode()) {
+      case AGENT -> {
+        var agent = Locator.$(new Agent(Integer.parseInt(agentId)));
+        if (agent==null) {
+          throw new NotFoundException("could not find agent with id [%s]".formatted(agentId));
+        }
+        var worker = Startup.router.getWorker(agent.getSid());
+        if (worker==null) {
+          throw new NotFoundException(
+            "could not find worker for agent [%d] with sid [%s]".formatted(agent.id, agentId));
+        }
+        if (WorkerState.from(worker)==WorkerState.AVAILABLE) {
+          respond(response, new VoiceResponse.Builder()
+            .dial(Status.watch(new Party(agent)).build())
+            .redirect(toVoicemail(
+              "%s is not available. Please leave a message and your call will be returned as soon as possible"))
+            .build());
+        } else {
+          respond(response, new VoiceResponse.Builder()
+            .redirect(toVoicemail(
+              "%s is not available. Please leave a message and your call will be returned as soon as possible"))
+            .build());
+        }
+      }
+      case NUMBER -> {
         var vCid = Locator.$1(VerifiedCallerId.isDefault);
+        var number = param.value();
         respond(response, new VoiceResponse.Builder()
           .dial(Status.watch(new Party(new PhoneNumber(number))).callerId(vCid.getPhoneNumber()).build())
           .hangup(hangup)
           .build());
       }
-    } else {
-      var agent = Locator.$(new Agent(Integer.parseInt(agentId)));
-      if (agent==null) {
-        throw new NotFoundException("could not find agent with id [%s]".formatted(agentId));
-      }
-      var worker = Startup.router.getWorker(agent.getSid());
-      if (worker==null) {
-        throw new NotFoundException("could not find worker for agent [%d] with sid [%s]".formatted(agent.id, agentId));
-      }
-
-      if (WorkerState.from(worker) == WorkerState.AVAILABLE) {
-        respond(response, new VoiceResponse.Builder()
-          .dial(Status.watch(new Party(agent)).build())
-          .redirect(
-            toVoicemail("%s is not available. Please leave a message and your call will be returned as soon as possible"))
-          .build());
-      } else {
-        respond(response, new VoiceResponse.Builder()
-          .redirect(
-            toVoicemail("%s is not available. Please leave a message and your call will be returned as soon as possible"))
-          .build());
-
+      case QUEUE -> {
+        var skillQueue = Locator.$(new SkillQueue(Integer.parseInt(param.value())));
+        var toQueue = new VoiceResponse.Builder();
+        var caller = Party.fromRequest(request, "Caller");
+        Locator.update(call, "Transfer", copy -> {
+          Inbound.enqueue(toQueue, caller, call, skillQueue, call.getSource());
+        });
+        respond(response, toQueue.build());
       }
     }
   }
