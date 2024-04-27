3b813860f363199c573ef97af4100db3c012e962
Michael Ventura <michael.ventura@gmail.com>
2024-04-26 23:35:04 -0400

    fix role names in getQueue

--- src/main/java/com/ameriglide/phenix/twilio/menu/Main.java
+++ src/main/java/com/ameriglide/phenix/twilio/menu/Main.java
@@ -59,7 +59,7 @@ public class Main extends Menu.Step {
               + "the best"
               + " solution, at the lowest possible price. We guarantee it! Ask your mobility specialist about our 110% "
               + "guarantee!"));
-          Inbound.enqueue(builder, caller, copy, router.getQueue("customer-service"),
+          Inbound.enqueue(builder, caller, copy, router.getQueue("customerService"),
             ProductLine.undetermined.get(),Source.PHONE);
         });
         return builder.build();
