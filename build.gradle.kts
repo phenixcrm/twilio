8d4061b7a698e484f8edc94a4c76c07262b4f1c1
Michael Ventura <michael.ventura@gmail.com>
2025-04-25 14:16:39 -0400

    switch everything to kotlin and used libs.toml

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+  war
+}
+
+dependencies {
+  testImplementation(
+    "org.junit.jupiter:junit-jupiter-api:5.9.0"
+  )
+  testRuntimeOnly(
+    "org.junit.jupiter:junit-jupiter-engine:5.9.0"
+  )
+  implementation(project(":common"))
+  implementation(project(":core"))
+  implementation(project(":potion"))
+  implementation(project(":validation"))
+  implementation(project(":types"))
+  implementation(project(":util"))
+  implementation(project(":cli"))
+  implementation(project(":sql"))
+  implementation(libs.postgresql)
+  implementation(libs.servlet)
+  implementation(libs.websocket)
+  implementation(libs.twilio)
+  implementation(libs.redis)
+  implementation(libs.dotenv)
+  implementation(libs.csv)
+
+}
